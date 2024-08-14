f24830638586c5381fe3c026758e388452f65379
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2024-08-14 20:04:26 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+jsx
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Us</h2>
+      <p>Welcome to The Brewberry ! We're a warm and inviting spot for coffee, treats, and community.</p>
+    </section>
+  );
+}
+
+export default About;
